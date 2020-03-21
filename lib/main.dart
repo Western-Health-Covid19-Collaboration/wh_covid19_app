a7a067783dc7e7d49d9828b2c8096415bbf3c48d
Alessandro Favero <alemalm19@gmail.com>
2020-03-22 09:31:00 +1100

    Restructured view folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wh_covid19/view/home_page.dart';
-import 'package:wh_covid19/view/ppe_view.dart';
+import 'package:wh_covid19/view/ppe/ppe_view.dart';
 import 'package:wh_covid19/view/sbs_guide_view.dart';
-import 'package:wh_covid19/view/staff_welfare_view.dart';
+import 'package:wh_covid19/view/staff_welfare/staff_welfare_view.dart';
 
 void main() => runApp(MyApp());
 
