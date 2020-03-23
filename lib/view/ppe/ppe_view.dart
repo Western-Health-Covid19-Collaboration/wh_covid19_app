41da6cea2786da21f0f9de3a53b280a9d252d980
Adam Koch <adamkoch@users.noreply.github.com>
2020-03-23 15:23:14 +1100

    Improve PPE view (#23)
    * Improve PPE view
    
    * Update ppe_view.dart

--- lib/view/ppe/ppe_view.dart
+++ lib/view/ppe/ppe_view.dart
@@ -1,23 +1,74 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'package:wh_covid19/style.dart';
 import 'package:wh_covid19/widget/reusable_card.dart';
 
-class PPEView extends StatelessWidget {
-  final title = 'PPE';
-  final color = Color.fromRGBO(255, 255, 255, 0.94);
+class PPEView extends StatefulWidget {
+  static final _cardHeight = 34.0;
 
-  final cards = <ReusableCard>[
+  @override
+  _PPEViewState createState() => _PPEViewState();
+}
+
+class _PPEViewState extends State<PPEView> {
+  final title = 'PPE';
+
+  final color = Color.fromRGBO(255, 255, 255, 0.94);
+  VideoPlayerController _videoController1;
+  ChewieController _chewieController1;
+
+  VideoPlayerController _videoController2;
+  ChewieController _chewieController2;
+
+  final puttingOnCards = <ReusableCard>[
     ReusableCard(
-      title: 'Putting On',
-      description: '5 steps',
-      color: Colors.white,
+      title: 'Step By Step Guide',
+      color: backgroundGreen,
+      height: PPEView._cardHeight,
     ),
     ReusableCard(
-      title: 'Taking Off',
-      description: 'Description',
-      color: Colors.white,
+      title: 'Infographic',
+      color: backgroundGreen,
+      height: PPEView._cardHeight,
     )
   ];
+
+  final takingOffCards = <ReusableCard>[
+    ReusableCard(
+      title: 'Step By Step Guide',
+      color: backgroundBrown,
+      height: PPEView._cardHeight,
+    ),
+    ReusableCard(
+      title: 'Infographic',
+      color: backgroundBrown,
+      height: PPEView._cardHeight,
+    )
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController1 =
+        VideoPlayerController.asset('assets/videos/wh_icu_donning.mp4');
+    _chewieController1 = ChewieController(
+      videoPlayerController: _videoController1,
+      aspectRatio: 16 / 9,
+      autoPlay: false,
+      autoInitialize: true,
+      placeholder: Container(color: Colors.black),
+    );
+    _videoController2 =
+        VideoPlayerController.asset('assets/videos/wh_icu_doffing.mp4');
+    _chewieController2 = ChewieController(
+      videoPlayerController: _videoController2,
+      aspectRatio: 16 / 9,
+      autoPlay: false,
+      autoInitialize: true,
+      placeholder: Container(color: Colors.black),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,12 +85,33 @@ class PPEView extends StatelessWidget {
       body: Container(
         width: double.infinity,
         padding: EdgeInsets.all(12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: cards,
+        child: ListView(
+          children: [
+            _buildHeader('Putting on Personal Protective Equipment'),
+            Chewie(controller: _chewieController1),
+            ...puttingOnCards,
+            _buildHeader('Taking off Personal Protective Equipment'),
+            Chewie(controller: _chewieController2),
+            ...takingOffCards,
+          ],
         ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoController1.dispose();
+    _videoController2.dispose();
+    super.dispose();
+  }
+
+  Widget _buildHeader(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 24, bottom: 4),
+        child: Text(text, style: cardContainerTextStyle),
       ),
     );
   }
