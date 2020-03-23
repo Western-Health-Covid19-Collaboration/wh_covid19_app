fc76564b5333faab60bad83ef0f91a39388e0f9a
Dylan Lange <dylan_lange@live.com>
2020-03-23 21:19:53 +1100

    Change title for intubation guidance screen

--- lib/view/intubation/guidance/intubation_guidance_page.dart
+++ lib/view/intubation/guidance/intubation_guidance_page.dart
@@ -8,7 +8,7 @@ class IntubationGuidancePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Intubation Guidance'),
+        title: Text('Intubation Steps'),
       ),
       body: Container(
         color: AppColors.backgroundGreen,
