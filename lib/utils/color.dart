6fc299c2f72af3a8e55a698722ff2217b73fa797
Eric Jiang <ericjiang97@gmail.com>
2020-03-25 22:10:37 +1100

    rework generateIconColor function

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -2,24 +2,27 @@ import 'package:flutter/material.dart';
 
 Color generateIconColor(
     Color fromColor, Color toColor, double deltaPercentage) {
-  var fromRed = fromColor.red,
-      fromGreen = fromColor.green,
-      fromBlue = fromColor.blue;
-  var toRed = toColor.red, toGreen = toColor.green, toBlue = toColor.blue;
+  final fromRed = fromColor.red;
+  final fromGreen = fromColor.green;
+  final fromBlue = fromColor.blue;
 
-  var deltaRed = (fromRed - toRed).abs(),
-      deltaGreen = (fromGreen - toGreen).abs(),
-      deltaBlue = (fromBlue - toBlue).abs();
+  final toRed = toColor.red;
+  final toGreen = toColor.green;
+  final toBlue = toColor.blue;
 
-  var newRed = fromRed < toRed
-          ? fromRed + deltaRed * deltaPercentage
-          : fromRed - deltaRed * deltaPercentage,
-      newGreen = fromGreen < toGreen
-          ? fromGreen + deltaGreen * deltaPercentage
-          : fromGreen - deltaGreen * deltaPercentage,
-      newBlue = fromBlue < toGreen
-          ? fromBlue + deltaBlue * deltaPercentage
-          : fromBlue - deltaBlue * deltaPercentage;
+  final deltaRed = (fromRed - toRed).abs();
+  final deltaGreen = (fromGreen - toGreen).abs();
+  final deltaBlue = (fromBlue - toBlue).abs();
+
+  final newRed = fromRed < toRed
+      ? fromRed + deltaRed * deltaPercentage
+      : fromRed - deltaRed * deltaPercentage;
+  final newGreen = fromGreen < toGreen
+      ? fromGreen + deltaGreen * deltaPercentage
+      : fromGreen - deltaGreen * deltaPercentage;
+  final newBlue = fromBlue < toGreen
+      ? fromBlue + deltaBlue * deltaPercentage
+      : fromBlue - deltaBlue * deltaPercentage;
 
   return Color.fromRGBO(newRed.toInt(), newGreen.toInt(), newBlue.toInt(), 1);
 }
