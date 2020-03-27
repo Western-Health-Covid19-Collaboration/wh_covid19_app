6d03b5184fec6cf44649f830b9fc9cfef35c8225
Maksim Lin <maks@manichord.com>
2020-03-27 21:10:37 +1100

    test disclaimer accepted, use better pregs mocking

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,9 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wh_covid19/intro_router.dart';
 import 'package:wh_covid19/main.dart';
 
 void main() {
-  setUpAll(() {
-    const MethodChannel('plugins.flutter.io/shared_preferences')
-        .setMockMethodCallHandler((methodCall) async {
-      if (methodCall.method == 'getAll') {
-        return <String, dynamic>{}; // set initial values here if desired
-      }
-      return null;
-    });
-  });
-
   testWidgets('show IntroRouter on app start', (tester) async {
     await tester.pumpWidget(MyApp());
     expect(find.byType(IntroRouter), findsOneWidget);
@@ -21,10 +11,23 @@ void main() {
 
   testWidgets('show disclaimer if user has not previously accepted it',
       (tester) async {
+    SharedPreferences.setMockInitialValues(<String, dynamic>{});
     await tester.pumpWidget(MyApp());
 
     await tester.pumpAndSettle(Duration(seconds: 2));
 
     expect(find.text('Disclaimer and conditions of use'), findsOneWidget);
+  });
+
+  testWidgets('show homescreen if user has previously accepted disclaimer',
+      (tester) async {
+    SharedPreferences.setMockInitialValues(
+        <String, dynamic>{'flutter.disclaimer_first_view': true});
+
+    await tester.pumpWidget(MyApp());
+
+    await tester.pumpAndSettle(Duration(seconds: 2));
+
+    expect(find.text('Western Health'), findsOneWidget);
   });
 }
