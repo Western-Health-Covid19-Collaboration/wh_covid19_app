b890370284eca7d7d2feee621afd2c32381efc29
Eric Jiang <ericjiang97@gmail.com>
2020-03-27 19:51:28 +1100

    added @required to the PPEStepInfo constructor

--- lib/models/PPEStepInfo.dart
+++ lib/models/PPEStepInfo.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+
 class PPEStepInfo {
   final String image;
   final String text;
   final List<String> notes;
 
-  const PPEStepInfo({this.image, this.text, this.notes});
+  const PPEStepInfo(
+      {@required this.image, @required this.text, @required this.notes});
 }
