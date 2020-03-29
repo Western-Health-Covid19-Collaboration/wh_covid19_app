c7c83711927115fce14fc8cdcc8cd233c93852de
June Chen <june.chen@rea-group.com>
2020-03-29 22:48:52 +1100

    Add close and platform unique share icons

--- lib/view/reusable_infographic_page.dart
+++ lib/view/reusable_infographic_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../style.dart';
 import '../widget/reusable_zoomable_widget.dart';
 
@@ -8,23 +10,33 @@ class InfographicPage extends StatelessWidget {
   final String title;
   final String imageUrl;
   final Color backgroundColor;
+  final bool _isAndroid = Platform.isAndroid;
   
-  const InfographicPage({this.title, this.imageUrl, this.backgroundColor});
+  InfographicPage({this.title, this.imageUrl, this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
+        iconTheme: AppStyles.appBarIconTheme,
         title: Text(
           title,
           style: AppStyles.appBarTextStyle,
           ),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.adb),
+            icon: _isAndroid ? SvgPicture.asset(
+              'assets/images/icon/SVG/icon_share_android.svg',
+            ) : SvgPicture.asset(
+              'assets/images/icon/SVG/icon_share_ios.svg',
+            ),
           ),
         ],
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => Navigator.of(context).pop(null),
+        ),
       ),
       body: 
         ZoomablePhoto(imageUrl: imageUrl, backgroundColor: backgroundColor)
