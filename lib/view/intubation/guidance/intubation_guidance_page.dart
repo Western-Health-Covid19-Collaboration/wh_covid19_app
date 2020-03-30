0bcbc40dea433e65fad9237f41153a0802546630
Bramley <bramers@gmail.com>
2020-03-31 00:34:07 +1100

    Fill out intubation guidance screen

--- lib/view/intubation/guidance/intubation_guidance_page.dart
+++ lib/view/intubation/guidance/intubation_guidance_page.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
+import '../../view_templates/intubation_content_view_template.dart';
+import '../../../hard_data.dart';
+import '../../../models/intubation_guide.dart';
 import '../../../style.dart';
 
 class IntubationGuidancePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Intubation Guide'),
-      ),
-      body: Container(
-        color: AppColors.backgroundGreen,
-        child: const Center(
-          child: Text('TODO'),
+    return DefaultTabController(
+      length: intubationGuide.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.green50,
+          title: const Text('Intubation Guide'),
+          textTheme: Theme.of(context).textTheme,
+          iconTheme: IconThemeData(color: AppColors.appBarIcon),
+          bottom: TabBar(
+            labelColor: AppColors.blackAlpha800,
+            labelStyle: AppStyles.textSemiBold,
+            unselectedLabelColor: AppColors.blackAlpha800.withOpacity(0.32),
+            indicatorColor: AppColors.green900,
+            tabs: intubationGuide.map((e) => Tab(text: e.name)).toList(),
+            isScrollable: true,
+          ),
+        ),
+        body: TabBarView(
+          children: intubationGuide
+              .map((e) => IntubationContentViewTemplate(content: e))
+              .toList(),
         ),
       ),
     );
