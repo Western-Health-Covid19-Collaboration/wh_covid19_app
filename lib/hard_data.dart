7ed1270f1d4bafb49ae4c306100b7e3fa74aa470
June Chen <june.chen@rea-group.com>
2020-03-30 18:53:05 +1100

    Change the wordings

--- lib/hard_data.dart
+++ lib/hard_data.dart
@@ -26,20 +26,20 @@ const List<ReusableCard> staffWelfare = [
 // Intubation card list composition
 final List<ReusableCard> intubation = [
   const ReusableCard(
-    title: 'Step By Step Guidance',
-    description: '12 steps',
+    title: 'Intubation Guide',
+    description: 'Step by step guide',
     color: AppColors.backgroundGreen,
     routeTo: Routes.intubationGuidance,
   ),
   const ReusableCard(
-    title: 'Checklist',
-    description: '12 steps',
+    title: 'Intubation Checklist',
+    description: 'Checklist',
     color: AppColors.backgroundGreen,
     routeTo: Routes.intubationChecklist,
   ),
   const ReusableCard(
-    title: 'Infographics',
-    description: '12 steps',
+    title: 'Extubation',
+    description: 'Infographic',
     color: AppColors.backgroundGreen,
     routeTo: Routes.intubationAlgorithm,
   )
--- lib/view/intubation/guidance/intubation_guidance_page.dart
+++ lib/view/intubation/guidance/intubation_guidance_page.dart
@@ -8,7 +8,7 @@ class IntubationGuidancePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Intubation Steps'),
+        title: const Text('Intubation Guide'),
       ),
       body: Container(
         color: AppColors.backgroundGreen,
