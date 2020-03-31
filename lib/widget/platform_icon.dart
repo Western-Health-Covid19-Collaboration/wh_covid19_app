afd9090efbfe3e5a06fc8c3e14ae9e911226cae2
June Chen <june.chen@rea-group.com>
2020-04-01 00:20:57 +1100

    Polish

--- lib/widget/platform_icon.dart
+++ lib/widget/platform_icon.dart
@@ -2,20 +2,23 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class PlatformIcon extends StatelessWidget {
+class IconPaths {
   static const _baseIconPath = 'assets/images/icon/SVG/';
+  static String get sharePath => _shareIconPath[defaultTargetPlatform];
 
-  static const Map<TargetPlatform, String> shareIconPath = { 
+  static const Map<TargetPlatform, String> _shareIconPath = { 
     TargetPlatform.iOS: '${_baseIconPath}icon_share_ios.svg', 
     TargetPlatform.android: '${_baseIconPath}icon_share_android.svg'
   };
+}
 
-  final Map<TargetPlatform, String> iconPath;
+class PlatformIcon extends StatelessWidget {
+  final String iconPath;
 
   const PlatformIcon({this.iconPath});
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(iconPath[defaultTargetPlatform]);
+    return SvgPicture.asset(iconPath);
   }
 }
