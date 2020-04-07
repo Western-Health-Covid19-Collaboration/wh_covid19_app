c263b020e2403381ce27a418478f8d846858c217
June Chen <june.chen@rea-group.com>
2020-04-07 18:10:36 +1000

    Apply reusable checklist to intubation checklist

--- lib/widget/containers/intubation_checklist_container.dart
+++ lib/widget/containers/intubation_checklist_container.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../models/IntubationChecklist.dart';
 import '../../style.dart';
+import '../../widget/lists/checklist_card.dart';
 import '../lists/intubation_checklist_item.dart';
 
 class IntubationChecklistContainer extends StatelessWidget {
@@ -9,32 +10,29 @@ class IntubationChecklistContainer extends StatelessWidget {
 
   const IntubationChecklistContainer({this.checklist});
 
-  List<Widget> renderList() {
-    return checklist
-        .map(
-          (e) => IntubationChecklistItemWidget(
-            listItem: e,
+  List<Widget> __renderList() {
+  return checklist
+      .map((e) => ChecklistItemWidget(
+            content: IntubationChecklistCard(listItem: e),
             backgroundColor: AppColors.appBackground,
             selectedBackgroundColor: AppColors.green50,
-          ),
-        )
-        .toList();
+          ))
+      .toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.green500,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-        child: ListView(
-          children: [
-            Container(padding: const EdgeInsetsDirectional.only(top: 10)),
-            ...renderList(),
-            Container(padding: const EdgeInsetsDirectional.only(bottom: 10)),
-          ],
-        ),
-      ),
-    );
+        color: AppColors.green500,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+          child: ListView(
+            children: [
+              Container(padding: const EdgeInsetsDirectional.only(top: 10)),
+              ...__renderList(),
+              Container(padding: const EdgeInsetsDirectional.only(bottom: 10)),
+            ],
+          ),
+        ));
   }
 }
--- lib/widget/lists/intubation_checklist_item.dart
+++ lib/widget/lists/intubation_checklist_item.dart
@@ -3,21 +3,12 @@ import 'package:provider/provider.dart';
 
 import '../../models/IntubationChecklist.dart';
 import '../../style.dart';
-import '../../view/airway/checklist/intubation_checklist_page.dart';
-import '../../widget/cards/reusable_card_base.dart';
-import '../../widget/checkbox.dart';
 import '../../widget/lists/string_list.dart';
 
-class IntubationChecklistItemWidget extends StatelessWidget {
+class IntubationChecklistCard extends StatelessWidget {
   final IntubationChecklistItem listItem;
-  final Color backgroundColor;
-  final Color selectedBackgroundColor;
 
-  const IntubationChecklistItemWidget({
-    @required this.listItem,
-    this.backgroundColor = AppColors.grey50,
-    this.selectedBackgroundColor = AppColors.grey600,
-  });
+  const IntubationChecklistCard({this.listItem});
 
   Widget getList() {
     if (listItem.notes != null && listItem.notes.isNotEmpty) {
@@ -31,50 +22,20 @@ class IntubationChecklistItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final selectionState =
-        Provider.of<IntubationChecklistSelectionProvider>(context);
-    final isChecked = selectionState.isChecked(listItem);
-    return ReusableCardBase(
-      elevation: 0,
-      fallback: () {
-        // setState not needed, because Provider takes care of rebuilding.
-        selectionState.setChecked(
-          listItem,
-          checked: !isChecked,
-        );
-      },
-      padding: const EdgeInsets.all(16),
-      color: isChecked ? selectedBackgroundColor : backgroundColor,
-      child: <Widget>[
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            ChecklistCheckbox(
-              checked: isChecked,
-              onChecked: () {
-                selectionState.setChecked(listItem, checked: !isChecked);
-              },
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      listItem.title,
-                      style: Styles.textH4,
-                    ),
-                    getList()
-                  ],
-                ),
-              ),
-            )
-          ],
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            listItem.title,
+            style: Styles.textH4,
+          ),
+          getList()
+        ],
+      ),
     );
   }
 }
