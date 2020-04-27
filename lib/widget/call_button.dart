4980027f9d77bdcbb10784e1f96b95dd83725f91
June Chen <qijie19920618@gmail.com>
2020-04-27 22:52:16 +1000

    Add call button to welfare page.

--- lib/widget/call_button.dart
+++ lib/widget/call_button.dart
@@ -4,27 +4,26 @@ import '../utils/url_utils.dart';
 
 class CallButton extends StatelessWidget {
   final String _url;
-  final Color _colour;
+  final Color _color;
 
-  const CallButton(this._url, this._colour);
+  const CallButton(this._url, {Color color}): _color = color;
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
       constraints: const BoxConstraints.tightFor(width: 24, height: 24),
-      padding: const EdgeInsets.all(0),
       icon: Stack(
         children: <Widget>[
           Align(
             alignment: Alignment.center,
-            child: Icon(Icons.call, color: _colour),
+            child: Icon(Icons.call, color: _color),
           ),
           Align(
             alignment: Alignment.topRight,
             child: Icon(
               Icons.call_made,
               size: 16,
-              color: _colour
+              color: _color
             ),
           )
         ],
