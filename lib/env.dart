945e3735f393e8c6542a6b659300be1d1d7c0b67
June Chen <qijie19920618@gmail.com>
2020-05-09 20:13:30 +1000

    Add place holder for last updated time (#333)
    * Add place holder for last updated time
    
    * Add timestamp converter
    
    * Format
    
    Co-authored-by: June Chen <june.chen@rea-group.com>

--- lib/env.dart
+++ lib/env.dart
@@ -1,3 +1,6 @@
 /// NOTE: this file will be OVER-WRITTEN at build time with values
 /// from environment variables on the build/CI server
 const dsn = 'PROVIDED_AT_BUILD_TIME';
+// Last updated date is used in disclaimer page and CI updates it automatically
+// Leave this string empty before CI updates it
+const lastUpdatedTime = '';
