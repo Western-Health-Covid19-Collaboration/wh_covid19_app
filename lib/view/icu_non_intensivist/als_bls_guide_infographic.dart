293fe7fac21007ebf683bfae3f5b16b7ddd21324
greggmiller <greggmiller296@gmail.com>
2020-05-10 08:47:45 +1000

    Infographics updated for:
    Intubation guide
    
    ALS and BLS modifications
    ALS and BLS modifications also changed to a PDF viewer as was previously an image viewer.
    
    Only slight issue is PDF viewer was unable to accept mixture of images in portrait or landscape. It seems to automatically present image in portrait.
    I had a look at dev package online and there was no obvious fixes.
    The images in landscape could be improved in terms of aspect, however they will suffice for current version.

--- lib/view/icu_non_intensivist/als_bls_guide_infographic.dart
+++ lib/view/icu_non_intensivist/als_bls_guide_infographic.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 
 import '../../strings.dart';
 import '../../style.dart';
-import '../reusable_infographic_page.dart';
+import '../reusable_infographic_pdf.dart';
 
 class ALSGuideInfographicPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const InfographicPage(
+    return const ReusableInfographicPdf(
       title: Strings.alsBlsGuideInfographicTitle,
-      imageUrl: 'assets/images/infographics/bls-modifications.png',
+      pdfUrl:
+          'assets/images/infographics/10052020_BLS_ALS__PPE_Landscape_Infographic.pdf',
       backgroundColor: AppColors.blue500,
       toolBarColor: AppColors.blue50,
     );
